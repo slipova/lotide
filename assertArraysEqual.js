033b6cd14b4c1b94e998b9ae5f3b87e02828b71f
Sofiya Lipova <sofiya.lipova@mail.mcgill.ca>
2021-10-27 02:55:05 +0000

    correct stylistic discrepancies

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,14 +1,11 @@
-
-
 const assertArraysEqual = function(array1, array2) {
   for (let i of array1) {
-    if (array1[i] !== array2[i] || array1.length !== array2.length)) {
+    if (array1[i] !== array2[i] || array1.length !== array2.length) {
       console.log(`❌❌❌Assertion Failed: ${array1} and ${array2} are not equal.`);
       return;
     }
   }
   console.log(`✅✅✅ Assertion Passed: ${array1} and ${array2} are equal.`);
 };
-
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
