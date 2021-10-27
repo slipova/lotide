421650549607a3c9ea7d8df8c10a08eda9ba89f2
Sofiya Lipova <sofiya.lipova@mail.mcgill.ca>
2021-10-27 19:46:19 +0000

    fix conditionals, test manually

--- countLetters.js
+++ countLetters.js
@@ -1,26 +1,25 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+//DON'T KNOW HOW TO TEST IT WITH assertEqual =(
+
+// const assertEqual = function(actual, expected) {
+//   if (actual === expected) {
+//     console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+//   } else {
+//     console.log(`❌❌❌Assertion Failed: ${actual} !== ${expected}`);
+//   }
+// };
+
 
 const countLetters = function(mystring) {
   let newString = mystring.split(" ").join("");
   const letterObj = {};
-  console.log(newString);
   for (let letter of newString) {
-    console.log(letter);
+    if (!letterObj[letter]) {
+      letterObj[letter] = 1;
+    } else {
+      letterObj[letter] += 1;
+    }
   }
-}
+  return letterObj;
+};
 
-console.log(countLetters("lighthouse in the house"));
-
-/*
-1.! receive a string in a function
-2a.! remove spaces
-2b.! declare an object that will collect answers
-3. loop through the resulting string
-4. each time a character appears, it creates a key in the object
-*/
+countLetters("lighthouse in the house");
