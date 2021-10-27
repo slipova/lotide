3cb2a2b291aa6bf2c9feb64481118bcd1353c530
Sofiya Lipova <sofiya.lipova@mail.mcgill.ca>
2021-10-27 02:27:29 +0000

    modify conditional to include array length comparison

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -2,7 +2,7 @@
 
 const assertArraysEqual = function(array1, array2) {
   for (let i of array1) {
-    if (array1[i] !== array2[i]) {
+    if (array1[i] !== array2[i] || array1.length !== array2.length)) {
       console.log(`❌❌❌Assertion Failed: ${array1} and ${array2} are not equal.`);
       return;
     }
@@ -12,6 +12,3 @@ const assertArraysEqual = function(array1, array2) {
 
 
 assertArraysEqual([1, 2, 3], [1, 2, 3]); // => should PASS
-
-
-
