157fd07d88f93e2e079692e04be00d762a8d191c
Sofiya Lipova <sofiya.lipova@mail.mcgill.ca>
2021-10-27 18:50:34 +0000

    add loop through letters of the string

--- countLetters.js
+++ countLetters.js
@@ -7,16 +7,20 @@ const assertEqual = function(actual, expected) {
 };
 
 const countLetters = function(mystring) {
-  let newstring = mystring.split(" ").join("");
-  console.log(newstring);
+  let newString = mystring.split(" ").join("");
+  const letterObj = {};
+  console.log(newString);
+  for (let letter of newString) {
+    console.log(letter);
+  }
 }
 
 console.log(countLetters("lighthouse in the house"));
 
 /*
 1.! receive a string in a function
-2a. remove spaces
-2b. declare an object that will collect answers
+2a.! remove spaces
+2b.! declare an object that will collect answers
 3. loop through the resulting string
 4. each time a character appears, it creates a key in the object
 */
