23ddba57ed8a04e122d89686f4e2992f71e3216b
Sofiya Lipova <sofiya.lipova@mail.mcgill.ca>
2021-11-01 18:35:17 +0000

    refactor files

--- test/headTest.js
+++ test/headTest.js
@@ -1,5 +1,7 @@
-const assertEqual = require('../assertEqual');
+const head = require('../head')
+const assertEqual = require('../assertEqual')
 
 assertEqual(head([5, 6, 7]), 5);
 assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
 assertEqual(head([]), "");
+
